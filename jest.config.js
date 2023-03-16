892f7e99fb1f249e86ad5f39c11d6f3f9ec3cd1f
lfernando <fernandocostadev98@gmail.com>
2023-03-16 18:13:14 -0300

    chore: add covverage threshold

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,20 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['./src/jest.setup.ts'],
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  covarageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+  setupFilesAfterEnv: ["./src/jest.setup.ts"],
+
   globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.jest.json',
+    "ts-jest": {
+      tsconfig: "tsconfig.jest.json",
     },
   },
 };
